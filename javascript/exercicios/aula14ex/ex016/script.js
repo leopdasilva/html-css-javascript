9a5c1601d73fe6990d0a9dfe5efcbc6f77a8b611
LS <leonardopereiradasilva774@gmail.com>
2025-09-24 20:39:44 -0300

    Correção tabuada JavaScript

--- javascript/exercicios/aula14ex/ex016/script.js
+++ javascript/exercicios/aula14ex/ex016/script.js
@@ -1,18 +1,23 @@
+const { createElement } = require("react");
+
 function tabuada() {
-    var tab = window.document.getElementById('txttab')
-    var res = document.getElementById('campo-tabuada')
+    var num = window.document.getElementById('txtn')
+    var tab = window.document.getElementById('seltab')
 
-    if (tab.value.length == 0) {
+    if (num.value.length == 0) {
         window.alert('Por favor, digite um número!')
-        return;
+    } else {
+        var n = Number(num.value)
+        tab.innerHTML = ''
+        for (var i = 1; i <= 10; i++) {
+            var item = document.createElement('option')
+            item.text = `${n} x ${i} = ${n * i}`  
+            item.value = `tab${i}` 
+            tab.appendChild(item)
+            
+        }
     }
 
-    res.innerHTML = ''
-    var tabval = Number.parseInt(tab.value)
-    var mult = 0
-
-    for (var i = 1; i <= 10; i++) {
-        mult = tabval * i
-        res.innerHTML += `${tabval} x ${i}  = ${mult}\n`
-    }
 }
+
+  
